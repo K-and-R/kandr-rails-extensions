ca27201f965b935424e853b42154b832bf299507
Karl Wilbur <karl@kandrsoftware.com>
2026-07-08 14:59:00 -0400

    Add more tests

--- spec/hash_spec.rb
+++ spec/hash_spec.rb
@@ -3,4 +3,33 @@
 require 'spec_helper'
 
 describe Hash do
+  context 'using HashDrop' do
+    context 'checking `drop`' do
+      it 'returns new clone' do
+        hash = {foo: 'foo', bar: 'bar', baz: 'baz'}
+		expect(hash.drop(nil)).not_to be(hash)
+      end
+      it 'removes passed key from clone' do
+        hash = {foo: 'foo', bar: 'bar', baz: 'baz'}
+        expect(hash.drop(:bar).include?(:bar)).to be_false
+        expect(hash.include?(:foo)).to be_true
+        expect(hash.include?(:bar)).to be_true
+        expect(hash.include?(:baz)).to be_true
+      end
+      it 'removes passed keys from clone' do
+        hash = {foo: 'foo', bar: 'bar', baz: 'baz'}
+        expect(hash.drop([:bar, :baz]).include?(:bar)).to be_true
+        expect(hash.drop([:bar, :baz]).include?(:baz)).to be_true
+        expect(hash.include?(:foo)).to be_true
+        expect(hash.include?(:bar)).to be_true
+        expect(hash.include?(:baz)).to be_true
+      end
+    end
+    context 'checking `drop!`' do
+      it 'returns self' do
+        hash = {foo: 'foo', bar: 'bar', baz: 'baz'}
+		expect(hash.drop!(nil)).to be(hash)
+      end
+    end
+  end
 end
